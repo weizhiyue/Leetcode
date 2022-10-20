faa493573364186d49a9084be48a55465bb03227
zhiyue wei <74879184+weizhiyue@users.noreply.github.com>
2022-10-20 15:22:44 -0400

    Update 45 Jump Game II.py

--- Doordash/45 Jump Game II.py
+++ Doordash/45 Jump Game II.py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Greedy algorithm
+        # For each phase, select the step that can take you to the farthest point
+        farthest = 0
+        current_end = 0
+        jump_step = 0
+        
+        # Iteration
+        for i in range(len(nums) - 1):
+            # Update the farthest point
+            farthest = max(i + nums[i], farthest)
+            # When we reach the end of the current phase, need to jump another step
+            if i == current_end:
+                jump_step += 1
+                current_end = farthest
+        
+        return jump_step
