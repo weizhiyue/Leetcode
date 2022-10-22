43eda0b90e36cb93bddc1bd42bb91f9471384f6e
zhiyue wei <74879184+weizhiyue@users.noreply.github.com>
2022-10-22 00:24:29 -0400

    Update 1166 Design File System.py

--- Doordash/1166 Design File System.py
+++ Doordash/1166 Design File System.py
@@ -41,6 +41,69 @@ class FileSystem(object):
             return -1
         
 
+### Sol2: Trie
+# Create a class for trie node
+class TrieNode(object):
+    def __init__(self, name):
+        self.map = defaultdict(TrieNode)  # children
+        self.name = name
+        self.value = -1
+        
+class FileSystem(object):
+
+    def __init__(self):
+        # Initialize the root
+        self.root = TrieNode("")
+        
+    def createPath(self, path, value):
+        """
+        :type path: str
+        :type value: int
+        :rtype: bool
+        """
+        ### Create a new path and associate a value to it
+        # Split the path into several parts based on "/"
+        path_components = path.split("/")[1:]
+        print("components:", path_components)
+        
+        curr = self.root
+        for i in range(len(path_components)):
+            name = path_components[i]
+            if i == len(path_components) - 1:
+                if name not in curr.map:
+                    curr.map[name] = TrieNode(name)
+                    curr.map[name].value = value
+                    return True
+                else:
+                    return False  
+            else:
+                # Not the last component
+                if name not in curr.map:
+                    return False
+                else:
+                    # move on
+                    curr = curr.map[name]
+        return False
+        
+    def get(self, path):
+        """
+        :type path: str
+        :rtype: int
+        """
+        ### Returns the value associated with path
+        path_components = path.split("/")[1:]
+        print("components: ", path_components)
+        
+        curr = self.root
+        for i in range(len(path_components)):
+            name = path_components[i]
+            if name not in curr.map:
+                return -1
+            curr = curr.map[name]
+        return curr.value
+        
+        
+
 
 # Your FileSystem object will be instantiated and called as such:
 # obj = FileSystem()
